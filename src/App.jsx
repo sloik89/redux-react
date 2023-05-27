8f1869f78b301edc2bbbd28d4e61042db915a91b
sloik89 <sloik89@gmail.com>
2023-05-27 16:54:38 +0200

    adding modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CartItems from "./components/CartItems";
+import Modal from "./components/Modal";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 function App() {
@@ -12,6 +13,7 @@ function App() {
   }, [cartItems]);
   return (
     <>
+      <Modal />
       <Navbar />
       <CartItems />
     </>
